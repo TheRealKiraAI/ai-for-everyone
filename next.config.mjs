dbf613f78db9ac877bee4a38c3466f681f41ffdf
KiraCorbett <kiracorbett00@gmail.com>
2024-08-12 15:51:40 -0700

    static pages for gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  trailingSlash: true, // Adds trailing slash to routes
+  images: {
+    unoptimized: true, // Disable image optimization (not supported on GitHub Pages)
+  },
+};
 
 export default nextConfig;
