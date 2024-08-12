6923ba19f72bbca2331c5c4c07087a0d60056eff
KiraCorbett <kiracorbett00@gmail.com>
2024-08-12 16:13:43 -0700

    removed gh pages config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-const repoName = "ai-for-everyone"; // replace with your actual repo name
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: `/${repoName}`,
-  assetPrefix: `/${repoName}/`,
-  output: "export",
-  trailingSlash: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
